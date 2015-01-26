348f2f6ea01570771c889a7818ebb5e7b086d013
ncelton <ncelton@ippon.fr>
2015-01-26 17:23:52 +0100

    Add DAO for users

---
+++ project-application/src/main/java/com/example/todo/db/UserDAO.java
@@ -0,0 +1,29 @@
+package com.example.todo.db;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+
+import com.example.todo.core.User;
+import com.google.common.base.Optional;
+
+import io.dropwizard.hibernate.AbstractDAO;
+
+public class UserDAO extends AbstractDAO<User> {
+
+	public UserDAO(SessionFactory sessionFactory) {
+		super(sessionFactory);
+	}
+
+    public Optional<User> findById(Long id) {
+        return Optional.fromNullable(get(id));
+    }
+
+    public Optional<User> create(User user) {
+        return Optional.fromNullable(persist(user));
+    }
+
+    public List<User> findAll() {
+        return list(namedQuery("com.example.todo.core.User.findAll"));
+    }
+}
